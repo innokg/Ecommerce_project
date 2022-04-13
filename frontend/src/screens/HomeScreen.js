2079d68122671be4537a71128a0f89a28c33ad9a
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-04-13 21:37:52 +0600

    added some features

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 
 function HomeScreen() {
-  const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const {  error, loading, products } = productList
 
 
-  useEffect(()=>{
+    useEffect(() => {
 
-      async function fetchProducts(){
-        const { data } = await axios.get('/api/products/')
-        setProducts(data)
-      }
+        dispatch(listProducts())
 
-      fetchProducts()
-  }, [])
+    }, [])
+
 
   return (
     <div>
